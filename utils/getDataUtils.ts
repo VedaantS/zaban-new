1ca654788e627a14afd3c1878ef5336460fde47a
Vedaant Srivastava <91336264+VedaantS@users.noreply.github.com>
2024-02-19 18:23:04 -0500

    Update getDataUtils.ts

--- utils/getDataUtils.ts
+++ utils/getDataUtils.ts
@@ -1,7 +1,7 @@
 import { getCountryVariant, Language, CountryVariant } from './locales';
 import fetch from 'node-fetch';
 
-const BASE_API_URL = 'http://factfully.riverdale.edu:10000';
+const BASE_API_URL = 'http://factfully.riverdale.edu:10000/data';
 export const KIDS_CATEGORY_ID = 'recSHyEn6N0hAqUBp';
 
 /**
